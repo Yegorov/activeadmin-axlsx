cc7b771b922500c83607d44b50249502336fc736
Artem <yegorov0725@yandex.ru>
2019-03-28 10:41:20 +0300

    Fix

--- lib/active_admin/axlsx/resource_controller_extension.rb
+++ lib/active_admin/axlsx/resource_controller_extension.rb
@@ -11,7 +11,7 @@ module ActiveAdmin
 
       # patching the index method to allow the xlsx format.
       def index(&block)
-        index_without_xlsx do |format|
+        super do |format|
            format.xlsx do
             xlsx = active_admin_config.xlsx_builder.serialize(collection)
             send_data xlsx, :filename => "#{xlsx_filename}", :type => Mime::Type.lookup_by_extension(:xlsx)
